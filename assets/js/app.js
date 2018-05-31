ba98c8236d891df454eebcf91c7801092b0c4a01
Flavio Kleiber <flaverkleiber@yahoo.de>
2018-05-31 17:51:10 +0200

    Clean up done

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,17 @@ let axios = require('axios');
 class App {
     constructor(init) {
         this.init = init;
+        this.depend = {};
     }
     run() {
-        console.log('I will run now!', this.init);
+        console.log('I will run now!', this.init, this.depend);
+    }
+    add(name, object) {
+        this.depend[name] = object;
     }
 }
 
 var app = new App('test');
+app.add('ajax', axios);
 app.run();
 console.log(axios);
